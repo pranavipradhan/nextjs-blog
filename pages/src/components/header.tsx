b313dd74ac7f87ee6e0fd0ad03dd45904aabf195
Pranavi Pradhan <pranavi.pradhan@assurex.co>
2024-07-12 16:30:08 +0530

    header component

--- pages/src/components/header.tsx
+++ pages/src/components/header.tsx
@@ -1,23 +1,11 @@
 import React from "react"; // header.tsx
 
 const Header: React.FC = () => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <>
+            
+        </>
+    );
 };
 
 export default Header;
