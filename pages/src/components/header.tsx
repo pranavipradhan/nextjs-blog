1e13c9ca3b1eb33508b70d43429ef329dbf76418
Pranavi Pradhan <pranavi.pradhan@assurex.co>
2024-07-12 16:03:53 +0530

    folder structure defined

---
+++ pages/src/components/header.tsx
@@ -0,0 +1,23 @@
+import React from "react"; // header.tsx
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
