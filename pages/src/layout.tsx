e0415fce79da0115e2910ea17552d77dcc9784dc
Pranavi Pradhan <pranavi.pradhan@assurex.co>
2024-07-15 10:12:46 +0530

    hero banner

--- pages/src/layout.tsx
+++ pages/src/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header";
+import HeroBanner from "./components/heroBanner";
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
+      <HeroBanner />
       {children}
     </div>
   );
